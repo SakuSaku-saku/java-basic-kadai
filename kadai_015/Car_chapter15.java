c00d31cde2b41028d176f6bb7f3fa11c3ea7dea5
SakuSaku-saku <ribero1992@yahoo.co.jp>
2026-07-20 17:29:48 +0900

    kadai_015をアップロード

---
+++ kadai_015/Car_chapter15.java
@@ -0,0 +1,33 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+    private int gear = 1;   
+    private int speed = 10;
+
+    
+    public void changeGear(int afterGear) {
+    	
+    	System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+
+        gear = afterGear;  
+
+        if (gear == 1) {
+            speed = 10;
+        } else if (gear == 2) {
+            speed = 20;
+        } else if (gear == 3) {
+            speed = 30;
+        } else if (gear == 4) {
+            speed = 40;
+        } else if (gear == 5) {
+            speed = 50;
+        } else {
+            speed = 10; 
+        }
+    }
+
+    public void run() {
+        System.out.println("速度は時速" + speed + "kmです");
+    }
+}
